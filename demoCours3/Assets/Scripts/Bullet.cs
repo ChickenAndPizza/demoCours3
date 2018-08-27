290e24595b11de81a8c75edd7cb16933041959d8
B533P-01\533 <alessard97@hotmail.com>
2018-08-27 16:54:02 -0400

    Bonhomme et tirer des balles et graphiques

---
+++ demoCours3/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        Vector3 ajustedMovement = transform.TransformDirection(Vector3.forward); /* Bouge selon TON axe des X */
+        rigidBody.AddForce(ajustedMovement * 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ demoCours3/Assets/Scripts/FireGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+    [SerializeField] GameObject Bullet;
+    [SerializeField] GameObject BulletEmiter;
+    [SerializeField] float fireRate = 1;
+    // Use this for initialization
+    private float timerLastShot = 0;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerLastShot += Time.deltaTime;
+        if (timerLastShot > fireRate/4)
+        {
+            var fire1 = Input.GetAxis("Fire1");
+            if (fire1 > 0)
+            {
+                Instantiate(Bullet, BulletEmiter.transform.position, BulletEmiter.transform.rotation);
+                timerLastShot = 0;
+            }
+        }
+
+	}
+}
